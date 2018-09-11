1bd888ad5cb99c7ce4ae63b95ff7667484a87ddc
stshekh <stshekh@users.noreply.github.com>
2018-09-12 02:27:59 +0300

    Role service partly done

--- service/src/main/java/com/gmail/sshekh/dao/impl/RoleServiceImpl.java
+++ service/src/main/java/com/gmail/sshekh/dao/impl/RoleServiceImpl.java
@@ -11,6 +11,9 @@ import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import java.util.Collections;
+import java.util.List;
+
 
 public class RoleServiceImpl implements RoleService {
 
@@ -20,6 +23,69 @@ public class RoleServiceImpl implements RoleService {
     private RoleDao roleDao = new RoleDaoImpl(Role.class);
     private RoleDTOConverter roleDTOConverter = new RoleDTOConverter();
     private RoleConverter roleConverter = new RoleConverter();
+
+    @Override
+    public RoleDTO save(RoleDTO roleDTO) {
+        Session session = roleDao.getCurrentSession();
+        try {
+            Transaction transaction = session.getTransaction();
+            if (!transaction.isActive()) {
+                session.beginTransaction();
+            }
+            Role role = roleConverter.toEntity(roleDTO);
+            roleDao.create(role);
+            RoleDTO roleDTONew = roleDTOConverter.toDTO(role);
+            transaction.commit();
+            return roleDTONew;
+        } catch (Exception e) {
+            if (session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+            logger.error("Cannot save role", e);
+        }
+        return roleDTO;
+    }
+
+    @Override
+    public RoleDTO update(RoleDTO roleDTO) {
+        Session session = roleDao.getCurrentSession();
+        try {
+            Transaction transaction = session.getTransaction();
+            if (!transaction.isActive()) {
+                session.beginTransaction();
+            }
+            Role role = roleConverter.toEntity(roleDTO);
+            roleDao.update(role);
+            RoleDTO roleDTONew = roleDTOConverter.toDTO(role);
+            transaction.commit();
+            return roleDTONew;
+        } catch (Exception e) {
+            if (session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+        }
+        return roleDTO;
+    }
+
+    @Override
+    public void delete(RoleDTO roleDTO) {
+        Session session = roleDao.getCurrentSession();
+        try {
+            Transaction transaction = session.getTransaction();
+            if (!transaction.isActive()) {
+                session.beginTransaction();
+            }
+            Role role = roleConverter.toEntity(roleDTO);
+            roleDao.delete(role);
+            transaction.commit();
+            logger.info("Role " + role.getRoleName() + " was deleted");
+        } catch (Exception e) {
+            if (session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+            logger.error("Failed to delete role", e);
+        }
+    }
 
     @Override
     public RoleDTO findRoleById(Long id) {
@@ -39,5 +105,25 @@ public class RoleServiceImpl implements RoleService {
             logger.error("Failed to fetch users", e);
         }
         return null;
+    }
+
+    @Override
+    public List<RoleDTO> findAll() {
+        Session session = roleDao.getCurrentSession();
+        try {
+            Transaction transaction = session.getTransaction();
+            if (!transaction.isActive()) {
+                session.beginTransaction();
+            }
+            List<Role> roles = roleDao.findAll();
+            List<RoleDTO> roleDTOS = roleDTOConverter.toDTOList(roles);
+            transaction.commit();
+            return roleDTOS;
+        } catch (Exception e) {
+            if (session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+        }
+        return Collections.emptyList();
     }
 }
